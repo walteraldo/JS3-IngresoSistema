c598ae8e930d98d2b18b7251bc4a46b78608a2ed
wfalcone <wfalcone@osocna.com.ar>
2022-05-21 18:12:38 -0300

    ceracion form y validación

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
-const nombre = document.getElementById('usuario');
-const pass = document.getElementById('pass');
+let usuario = document.getElementById('usuario');
+let pass = document.getElementById('pass');
+const btn = document.getElementById('boton')
 
-const enviarFormulario = () => console.log('Enviando formulario...');
+btn.addEventListener('click', () => {
+  
+  if(usuario.value == ''){
+    usuario.setCustomValidity('Complete su nombre');
+  } else if(!usuario.value.includes("@")){
+    usuario.setCustomValidity('Debe contener un @');
+  } else if(pass.value == ""){
+    pass.setCustomValidity('Complete su contraseña');
+  } else {
+    usuario.setCustomValidity('');
+    pass.setCustomValidity('');
+  }
+});
