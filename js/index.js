bb89ef093c6bd8286d6646689080f528f6c1a7b0
wfalcone <wfalcone@osocna.com.ar>
2022-05-21 20:32:29 -0300

    arreglo setCustomValidity

--- js/index.js
+++ js/index.js
@@ -1,27 +1,28 @@
-let usuario = document.getElementById('usuario');
-let pass = document.getElementById('pass');
-const btn = document.getElementById('boton')
+const user = document.getElementById('username');
+const pass = document.getElementById('pass');
+const btn = document.getElementById('boton');
 
 btn.addEventListener('click', () => {
 
-  if(usuario.value == ''){
-    alert('Complete su nombre');
-  } else if(!usuario.value.includes("@")) {
-    alert('Debe contener un @');
-  } else if (pass.value == "") { 
-    alert('Complete su contraseña');
-  } else {
-    alert('Profe, no me funcionó el setCustomValid');
-  }
-});
-
-// if (pass.value == ""){
-//   pass.setCustomValid('Complete su contraseña');
-// } else if(usuario.value == ''){
-//   usuario.setCustomValid('Complete su nombre');
-// } else if(!usuario.value.includes("@")){
-//   usuario.setCustomValid('Debe contener un @');
-// } else {
-//   alert('Ingreso correcto');
-// }
+  // if(usuario.value == ''){
+  //   alert('Complete su nombre');
+  // } else if(!usuario.value.includes("@")) {
+  //   alert('Debe contener un @');
+  // } else if (pass.value == "") { 
+  //   alert('Complete su contraseña');
+  // } else {
+  //   alert('Profe, no me funcionó el setCustomValid');
+  // }
 // });
+
+  if(!user.value.includes("@")) {
+    user.setCustomValidity('Debe contener un @');
+  } else if(user.value == '') {
+    user.setCustomValidity('Complete su nombre');
+  } else if(pass.value == "") {
+    pass.setCustomValidity('Complete su contraseña');
+  } else {
+    alert('Ingreso correcto');
+  }
+
+});
