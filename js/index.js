d8b4c22bf5fa89a69cfc026d5ab54b7764cc7a16
wfalcone <wfalcone@osocna.com.ar>
2022-05-21 18:36:26 -0300

    arreglo alert

--- js/index.js
+++ js/index.js
@@ -2,16 +2,16 @@ let usuario = document.getElementById('usuario');
 let pass = document.getElementById('pass');
 const btn = document.getElementById('boton')
 
-btn.addEventListener('click', () => {
-  
-  if(usuario.value == ''){
-    usuario.setCustomValidity('Complete su nombre');
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+
+
+  if (pass.value == ""){
+    alert('Complete su contraseña');
+  } else if(usuario.value == ''){
+    alert('Complete su nombre');
   } else if(!usuario.value.includes("@")){
-    usuario.setCustomValidity('Debe contener un @');
-  } else if(pass.value == ""){
-    pass.setCustomValidity('Complete su contraseña');
-  } else {
-    usuario.setCustomValidity('');
-    pass.setCustomValidity('');
-  }
+    alert('Debe contener un @');
+  } 
 });
