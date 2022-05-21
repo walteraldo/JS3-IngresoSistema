0f3a7563c37dfddeb98f1039b9d8929b50f5834c
wfalcone <wfalcone@osocna.com.ar>
2022-05-21 19:29:50 -0300

    agrego bandera ingreso

--- js/index.js
+++ js/index.js
@@ -5,13 +5,24 @@ const btn = document.getElementById('boton')
 btn.addEventListener('click', (e) => {
   e.preventDefault();
 
+//   if (pass.value == ""){
+//     alert('Complete su contraseña');
+//   } else if(usuario.value == ''){
+//     alert('Complete su nombre');
+//   } else if(!usuario.value.includes("@")){
+//     alert('Debe contener un @');
+//   } else {
+//     alert('Ingreso correcto');
+//   }
+// });
 
-
-  if (pass.value == ""){
-    alert('Complete su contraseña');
-  } else if(usuario.value == ''){
-    alert('Complete su nombre');
-  } else if(!usuario.value.includes("@")){
-    alert('Debe contener un @');
-  } 
+if (pass.value == ""){
+  pass.setCustomValid('Complete su contraseña');
+} else if(usuario.value == ''){
+  usuario.setCustomValid('Complete su nombre');
+} else if(!usuario.value.includes("@")){
+  usuario.setCustomValid('Debe contener un @');
+} else {
+  alert('Ingreso correcto');
+}
 });
