1320ab2a453d11ff258353f315fc7347acb3cc45
wfalcone <wfalcone@osocna.com.ar>
2022-05-22 20:26:12 -0300

    resultado final

--- js/index.js
+++ js/index.js
@@ -3,17 +3,6 @@ const pass = document.getElementById('pass');
 const btn = document.getElementById('boton');
 
 btn.addEventListener('click', () => {
-  // e.preventDefault();
-  // if(usuario.value == ''){
-  //   alert('Complete su nombre');
-  // } else if(!usuario.value.includes("@")) {
-  //   alert('Debe contener un @');
-  // } else if (pass.value == "") { 
-  //   alert('Complete su contraseña');
-  // } else {
-  //   alert('Profe, no me funcionó el setCustomValid');
-  // }
-// });
 
   if(user.value == ''){
     user.setCustomValidity('Complete su nombre');
@@ -28,7 +17,4 @@ btn.addEventListener('click', () => {
     user.setCustomValidity('');
     pass.setCustomValidity('');
   }
-
-  
-
 });
